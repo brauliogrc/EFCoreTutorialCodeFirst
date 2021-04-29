7c18e5ca9b4bda7406963700e072aba924cd4768
brauliogrc <brauliogrc95@gmail.com>
2021-04-29 06:31:57 -0500

    Creation of ContextClass and entities Student and Course

---
+++ EFCoreTutorial2/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial2.Models
+{
+    class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
---
+++ EFCoreTutorial2/Program.cs
@@ -0,0 +1,20 @@
+﻿using EFCoreTutorial2.Models;
+using System;
+
+namespace EFCoreTutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { Name = "BraulioGrc" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Studen saved successfuly!");
+        }
+    }
+}
